bd4194f76464cc0ead1492b3f90f8f7f64fca6ef
SuperVader2015 <eyal.ktlk@gmail.com>
2022-10-02 10:45:43 +0300

    DO NO USE EXCEL

--- Data Processing/Initial_processing.py
+++ Data Processing/Initial_processing.py
@@ -4,8 +4,8 @@ import pandas
 def main():
     print("Hello World")
     datasheets = pandas.read_csv("Datasheets.csv", sep='|')
-    #datasheets.sort_values(datasheets.columns[0], axis=0, inplace=True)
-    #datasheets.to_csv("Datasheets.csv",sep="|")
+    datasheets.sort_values(datasheets.columns[0], axis=0, inplace=True)
+    datasheets.to_csv("Datasheets.csv",sep="|")
     line_count = 0
     print(datasheets)
 
