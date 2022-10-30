2b0cfdc7038b37afcaefb8887119fe5af42ea5d5
SuperVader2015 <eyal.ktlk@gmail.com>
2022-10-30 18:47:25 +0200

    models

--- Initial_processing.py
+++ Initial_processing.py
@@ -2,12 +2,47 @@ import pandas
 
 
 def main():
-    print("Hello World")
-    datasheets = pandas.read_csv("Datasheets.csv", sep='|')
-    datasheets.sort_values(datasheets.columns[0], axis=0, inplace=True)
-    datasheets.to_csv("Datasheets.csv",sep="|", index=False)
-    line_count = 0
-    print(datasheets)
+
+    # this part of the code is for processing the data from datasheets, primarly by removing unnessecary columns
+    # TO DO: process unit composition data
+    data = pandas.read_csv("Raw Data/Datasheets.csv", sep='|')
+    data.sort_values(data.columns[0], axis=0, inplace=True)
+    data.drop('link', inplace=True, axis=1)
+    data.drop('open_play_only', inplace=True, axis=1)
+    data.drop('crusade_only', inplace=True, axis=1)
+    data.drop('virtual', inplace=True, axis=1)
+    data.drop('power_points', inplace=True, axis=1)
+    data.drop('priest', inplace=True, axis=1)
+    data.drop('psyker', inplace=True, axis=1)
+    data.drop('transport', inplace=True, axis=1)
+    data.drop('Unnamed: 16', inplace=True, axis=1)
+    data.to_csv("Data Processing/Datasheets.csv", sep="|", index=False)
+
+    # what is "is_other_wargear"?
+    data = pandas.read_csv("Raw Data/Datasheets_abilities.csv", sep='|')
+    data.sort_values(data.columns[0], axis=0, inplace=True)
+    data.drop('is_index_wargear', inplace=True, axis=1)
+    data.to_csv("Data Processing/Datasheets_abilities.csv",
+                sep="|", index=False)
+
+    data = pandas.read_csv("Raw Data/Datasheets_keywords.csv", sep='|')
+    data.drop('Unnamed: 4', inplace=True, axis=1)
+    data.to_csv("Data Processing/Datasheets_keywords.csv",
+                sep="|", index=False)
+
+    data = pandas.read_csv("Raw Data/Datasheets_models.csv", sep='|')
+    data.drop('Unnamed: 18', inplace=True, axis=1)
+    data.drop('base_size', inplace=True, axis=1)
+    data.drop('base_size_descr', inplace=True, axis=1)
+    data.drop('M', inplace=True, axis=1)
+    data.drop('Ld', inplace=True, axis=1)
+    for col in data.columns:
+        if data[col].dtype == object:
+            data[col] = data[col].str.replace('[+"]', '')
+    data.to_csv("Data Processing/Datasheets_models.csv", sep="|", index=False)
+
+
+    print(data)
 
 
 if __name__ == "__main__":
